315759bcfc7a78d72543c3d9a1b21716887a32ee
BensonZhaoLangLi <63826923+BensonZhaoLangLi@users.noreply.github.com>
2020-08-12 06:38:57 -0400

    Input validation

--- formscripts.js
+++ formscripts.js
@@ -1,22 +1,71 @@
-window.onload = function(){
-    var questionBtn = document.getElementById("formTopic-1");
-    var commentBtn = document.getElementById("formTopic-2");
-    var orderBtn = document.getElementById("formTopic-3");
-    var orderNumberInput = document.getElementById("orderNumber");
+window.onload = function() {
+  var questionBtn = document.getElementById('formTopic-1');
+  var commentBtn = document.getElementById('formTopic-2');
+  var orderBtn = document.getElementById('formTopic-3');
+  var orderNumberInput = document.getElementById('orderNumber');
 
-    /*Removing and adding hidden attribute to order number input, depending on radio input*/
-    questionBtn.onclick = function(){
-        console.log("clicked");
-        orderNumberInput.classList.add("hidden");
+  /*Removing and adding hidden attribute to order number input, depending on radio input*/
+  questionBtn.onclick = function() {
+    console.log('clicked');
+    orderNumberInput.classList.add('hidden');
+  };
+
+  commentBtn.onclick = function() {
+    console.log('clicked');
+    orderNumberInput.classList.add('hidden');
+  };
+
+  orderBtn.onclick = function() {
+    console.log('clicked');
+    orderNumberInput.classList.remove('hidden');
+  };
+
+  /*Validation for contact us form*/
+  var contactForm = document.getElementById('contactForm');
+
+  contactForm.onsubmit = function() {
+    var errorMsg = document.getElementById('error');
+
+    /*remove error style on all input boxes, previously invalid input boxes will no longer be highlighted*/
+    var inputBoxes = document.contactForm.getElementsByTagName('input');
+    console.log(inputBoxes.length);
+    for (var i = 0; i < inputBoxes.length; i++) {
+      inputBoxes[i].classList.remove('error');
     }
 
-    commentBtn.onclick = function(){
-        console.log("clicked");
-        orderNumberInput.classList.add("hidden");
+    /*Validation for Canadian postal code: -A1A 1A1 (with or without space between) -can't contain  D, F, I, O, Q or U -required*/
+    var postalCode = document.contactForm.postalCode.value.trim();
+    var postalCodeRegex = /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/;
+
+    if (postalCode === '') {
+      document.contactForm.postalCode.classList.add('error');
+      errorMsg.innerHTML = '*Postal code is required.';
+      contactForm.postalCode.focus();
+      return false;
     }
 
-    orderBtn.onclick = function(){
-        console.log("clicked");
-        orderNumberInput.classList.remove("hidden");
+    if (!postalCodeRegex.test(postalCode)) {
+      document.contactForm.postalCode.classList.add('error');
+      errorMsg.innerHTML = '*Postal code invalid format.';
+      contactForm.postalCode.focus();
+      return false;
     }
-}
+
+    /*validation for orderNo: -required -must only contain numbers*/
+    var orderNo = document.contactForm.orderNumber.value.trim();
+
+    if (orderNo === '') {
+      document.contactForm.orderNumber.classList.add('error');
+      errorMsg.innerHTML = '*Order number is required.';
+      return false;
+    }
+
+    if (parseInt(orderNo) !== orderNo) {
+      document.contactForm.orderNumber.classList.add('error');
+      errorMsg.innerHTML = '*Order number must only contain numbers.';
+      return false;
+    }
+
+    return true;
+  };
+};
